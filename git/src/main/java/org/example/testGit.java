060a5081359934eadb209dc931b131c31eab6c6f
lgs <2317005619@qq.com>
2022-07-30 09:46:31 +0800

    third commit

--- git/src/main/java/org/example/testGit.java
+++ git/src/main/java/org/example/testGit.java
@@ -4,5 +4,6 @@ public class testGit {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
