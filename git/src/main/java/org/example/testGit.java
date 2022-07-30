8aa2b37acce22f01a137183b26ed3ba8eb91e1a6
lgs <2317005619@qq.com>
2022-07-30 10:35:48 +0800

    push commit

--- git/src/main/java/org/example/testGit.java
+++ git/src/main/java/org/example/testGit.java
@@ -8,5 +8,6 @@ public class testGit {
         System.out.println("hello hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
